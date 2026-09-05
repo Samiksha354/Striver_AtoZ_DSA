2531b9ff9512c589dec8450928d2ae566a44aa18
samiksha jadhav <samikshajadhav354@gmail.com>
2026-09-05 09:23:23 +0530

    chnages

--- Array/Medium/Kadanes_Algo_Max_Subarray_Sum.java
+++ Array/Medium/Kadanes_Algo_Max_Subarray_Sum.java
@@ -5,26 +5,67 @@ import static java.util.Collections.max;
 public class Kadanes_Algo_Max_Subarray_Sum {
 
     /// Brut force
-    static int maxsubarray(int[] arr,int m){
+    static int maxsubarray(int[] arr){
         int len=0;
         for(int i=0;i<arr.length;i++){
            for (int j=i;j<arr.length;j++){
-               int sum = 0;
+               int sum=0;
                for(int k=i;k<=j;k++){
-                  sum =arr[k]+sum;
-                  if(sum == m) {
-                      len=Math.max(len,j-i+1);
-                  }
+                   sum =arr[k]+sum;
                }
+               len=Math.max(len,sum);
            }
         }
         return len;
     }
+
+
+
+    /// Brut force
+    static int maxsubarray2(int[] arr){
+        int maxlen=Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            int sum = 0;
+            for (int j=i;j<arr.length;j++){
+                sum =arr[j]+sum;
+
+                maxlen=Math.max(maxlen,sum);
+
+
+            }
+        }
+        return maxlen;
+    }
+
+
+
+    /// ////////Kadanes Algoritham
+    static int maxsubarray3(int[] arr){
+        int  maxlen=Integer.MIN_VALUE;
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+          sum=sum+arr[i];
+          if(sum > maxlen){
+              maxlen =sum;
+          }
+          if(sum <0) sum =0;
+        }
+        return maxlen;
+    }
+
+
+
     public static void main(String [] args){
-        int[] arr={1 ,2,3,1,1,1,1,4,2,3};
-        int k=3;//sum that we want
-        int ans=maxsubarray(arr,k);
-        System.out.println(ans);
+        int[] arr={-2, -3, 4, -1,-2 ,1 ,5,-3};
+
+        int ans=maxsubarray(arr);
+        System.out.println("3Approach"+ans);
+
+        int ans1=maxsubarray2(arr);
+        System.out.println("2Approach"+ans1);
+
+        int ans2=maxsubarray3(arr);
+        System.out.println("3Approach"+ans2);
 
     }
 }
